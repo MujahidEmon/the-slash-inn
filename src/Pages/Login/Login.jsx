a17d8f853ab22aef902ae4a784e6361d0dc93456
MujahidEmon <mujahidulislam.official@gmail.com>
2025-07-26 10:27:58 +0600

    fixed some error in login form

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,9 +2,12 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import useAuth from "../../Hooks/useAuth";
 import toast from "react-hot-toast";
 import axios from "axios";
+import { useState } from "react";
+import { BsEye, BsEyeSlash } from "react-icons/bs";
 
 const Login = () => {
-    const {login} = useAuth();
+    const [showPass, setShowPass] = useState(false);
+    const { login } = useAuth();
     const location = useLocation();
     const navigate = useNavigate();
     const handleLogin = (e) => {
@@ -13,19 +16,19 @@ const Login = () => {
         const email = form.get("email");
         const password = form.get("password");
         login(email, password)
-        .then(res => {
-            console.log(res.user);
-            axios.post(`${import.meta.env.VITE_API_URL}/jwt`, {email: res.user.email}, {withCredentials: true})
             .then(res => {
-                console.log(res.data);
-            })
-            navigate(location?.state ? location?.state : '/');
+                console.log(res.user);
+                axios.post(`${import.meta.env.VITE_API_URL}/jwt`, { email: res.user.email }, { withCredentials: true })
+                    .then(res => {
+                        console.log(res.data);
+                    })
+                navigate(location?.state ? location?.state : '/');
 
-        })
-        .catch(error => {
-            console.log(error);
-            toast.error(error.message)
-        })
+            })
+            .catch(error => {
+                console.log(error);
+                toast.error(error.message)
+            })
     }
 
     return (
@@ -37,7 +40,7 @@ const Login = () => {
                     <div className="p-6 sm:p-8 rounded-2xl bg-white border border-gray-200 shadow-sm">
                         <h1 className="text-slate-900 text-center text-3xl font-semibold">Sign in</h1>
                         <form onSubmit={handleLogin}
-                         className="mt-12 space-y-6">
+                            className="mt-12 space-y-6">
                             <div>
                                 <label className="text-slate-900 text-sm font-medium mb-2 block">
                                     Email
@@ -69,20 +72,14 @@ const Login = () => {
                                 <div className="relative flex items-center">
                                     <input
                                         name="password"
-                                        type="password"
+                                        type={showPass ? "text" : "password"}
                                         required
                                         className="w-full text-slate-900 text-sm border border-slate-300 px-4 py-3 pr-8 rounded-md outline-blue-600"
                                         placeholder="Enter password"
                                     />
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="#bbb"
-                                        stroke="#bbb"
-                                        className="w-4 h-4 absolute right-4 cursor-pointer"
-                                        viewBox="0 0 128 128"
-                                    >
-                                        <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" />
-                                    </svg>
+                                    <button className="absolute right-4" onClick={() => {setShowPass(!showPass)}}>
+                                        {showPass ? <BsEyeSlash></BsEyeSlash> : <BsEye></BsEye>}
+                                    </button>
                                 </div>
                             </div>
 
